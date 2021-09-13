577a6b6b6e241f34d949bcef1bc21fec4cc1da42
Andrew <andrewmgunn26@gmail.com>
2021-09-13 12:19:28 -0500

    You can declare variables in TryParse( , int out max) - they work within local scope

--- Program.cs
+++ Program.cs
@@ -4,9 +4,36 @@ namespace Module5Lecture
 {
     class Program
     {
+        static void Lecture4Review()
+        {
+            //Counting program - menu with sentinel
+
+            string input = "";
+
+            do
+            {
+                Console.Write("U.  Count Up\nD.  Count Down\nE.  Exit\n>");
+                input = Console.ReadLine();
+
+                Console.WriteLine(input);
+
+                switch (input)
+                {
+                    case "U":
+                    case "u":
+                        Console.Write("Enter value to count up from 1>");
+                        Int32.TryParse(Console.ReadLine(), out int max);
+
+                        for (int i = 0; i <= max; i++)
+                        {
+                            
+                        }
+                }
+            } while (input != "E");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
