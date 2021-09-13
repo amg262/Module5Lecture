2e1002ad157f911fe30109c36f3028ce831256ab
Andrew <andrewmgunn26@gmail.com>
2021-09-13 12:27:02 -0500

    finished lecture 4 review code - i know this stuff

--- Program.cs
+++ Program.cs
@@ -7,33 +7,52 @@ namespace Module5Lecture
         static void Lecture4Review()
         {
             //Counting program - menu with sentinel
-
             string input = "";
 
             do
             {
                 Console.Write("U.  Count Up\nD.  Count Down\nE.  Exit\n>");
-                input = Console.ReadLine();
+                input = Console.ReadLine().Trim().ToUpper();
 
                 Console.WriteLine(input);
 
                 switch (input)
                 {
                     case "U":
-                    case "u":
                         Console.Write("Enter value to count up from 1>");
                         Int32.TryParse(Console.ReadLine(), out int max);
 
                         for (int i = 0; i <= max; i++)
                         {
-                            
+                            Console.Write(i + "\t");
                         }
+
+                        break;
+                    case "D":
+                        Console.Write("Enter value to count up from 10>");
+                        Int32.TryParse(Console.ReadLine(), out int min);
+
+                        while (min >= 10)
+                        {
+                            Console.WriteLine("Enter valid # less than 10");
+                            //Int32.TryParse(Console.ReadLine(), out min);
+                            min = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        for (int i = 0; i <= min; i--)
+                        {
+                            Console.Write(i + "\t");
+                        }
+
+                        break;
+                    default: break;
                 }
             } while (input != "E");
         }
 
         static void Main(string[] args)
         {
+            Lecture4Review();
         }
     }
 }
